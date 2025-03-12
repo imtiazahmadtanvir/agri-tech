a154abca0b28f1aab39aad1aebc7050742da777a
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-03-13 02:00:06 +0600

    feat(components): refactor TopInfoBar layout and add Container component for max -weight

--- src/components/shared/TopInfoBar.tsx
+++ src/components/shared/TopInfoBar.tsx
@@ -1,74 +1,77 @@
 import Image from "next/image";
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import Container from "./max-w-container/Container";
 
 const TopInfoBar = () => {
   return (
-    <div className="bg-[#0D401C] flex items-center px-4 justify-between text-white py-5">
-      <div className="bg-[#235130] w-fit flex items-center gap-4 py-1 px-2 rounded-2xl">
-        <h5>Welcome to AgriTech</h5>
-        <span className="size-1.5 bg-[#F8C32C] rounded-full"></span>
-        <h5>Agriculture & Organic Farms </h5>
-      </div>
-      <div className="flex items-center gap-14">
-        {/* contract info */}
-        <div className="flex items-center gap-4">
-          <div className="flex items-center gap-2 text-sm">
-            <div className="p-2.5 bg-[#F8C32C] rounded-full w-fit">
-              <Image
-                width={15}
-                height={15}
-                alt="phone icon"
-                src={"/icons/phone.svg"}
-              ></Image>
-            </div>
-            <p>
+    <section className="bg-[#0D401C] ">
+      <Container className="flex items-center px-4 justify-between text-white py-5">
+        <div className="bg-[#235130] w-fit flex items-center gap-4 py-1 px-2 rounded-2xl">
+          <h5>Welcome to AgriTech</h5>
+          <span className="size-1.5 bg-[#F8C32C] rounded-full"></span>
+          <h5>Agriculture & Organic Farms </h5>
+        </div>
+        <div className="flex items-center gap-14">
+          {/* contract info */}
+          <div className="flex items-center gap-4">
+            <div className="flex items-center gap-2 text-sm">
+              <div className="p-2.5 bg-[#F8C32C] rounded-full w-fit">
+                <Image
+                  width={15}
+                  height={15}
+                  alt="phone icon"
+                  src={"/icons/phone.svg"}
+                  style={{ width: "auto", height: "auto" }}
+                ></Image>
+              </div>
+
               <p>+1 987 654 3210</p>
-            </p>
-          </div>
-          <div className="flex items-center gap-2">
-            <div className="p-2.5 bg-[#F8C32C] rounded-full w-fit">
-              <Image
-                width={15}
-                height={15}
-                alt="phone icon"
-                src={"/icons/mail.svg"}
-              ></Image>
             </div>
-            <p>
+            <div className="flex items-center gap-2">
+              <div className="p-2.5 bg-[#F8C32C] rounded-full w-fit">
+                <Image
+                  width={15}
+                  height={15}
+                  alt="phone icon"
+                  src={"/icons/mail.svg"}
+                  style={{ width: "auto", height: "auto" }}
+                ></Image>
+              </div>
+
               <p>Donalfarms@gmail.com</p>
-            </p>
-          </div>
-          <div className="flex items-center gap-2">
-            <div className="px-2.5 py-1.5 bg-[#F8C32C] rounded-full w-fit">
-              <Image
-                width={15}
-                height={15}
-                alt="phone icon"
-                src={"/icons/location.svg"}
-              ></Image>
             </div>
-            <p>
+            <div className="flex items-center gap-2">
+              <div className="px-2.5 py-2 bg-[#F8C32C] rounded-full w-fit">
+                <Image
+                  width={15}
+                  height={15}
+                  alt="phone icon"
+                  style={{ width: "auto", height: "auto" }}
+                  src={"/icons/location.svg"}
+                ></Image>
+              </div>
+
               <p>Prinsengracht 250, Amsterdam Netherlands</p>
-            </p>
+            </div>
+          </div>
+          {/* social icon */}
+          <div className="bg-[#235130] px-6 py-3 w-fit flex items-center rounded-3xl gap-4">
+            <button>
+              <FaFacebook className="text-[#8A9B8E]" />
+            </button>
+            <button>
+              <FaTwitter className="text-[#8A9B8E]" />
+            </button>
+            <button>
+              <FaLinkedin className="text-[#8A9B8E]" />
+            </button>
+            <button>
+              <FaInstagram className="text-[#8A9B8E]" />
+            </button>
           </div>
         </div>
-        {/* social icon */}
-        <div className="bg-[#235130] px-6 py-3 w-fit flex items-center rounded-3xl gap-4">
-          <button>
-            <FaFacebook className="text-[#8A9B8E]" />
-          </button>
-          <button>
-            <FaTwitter className="text-[#8A9B8E]" />
-          </button>
-          <button>
-            <FaLinkedin className="text-[#8A9B8E]" />
-          </button>
-          <button>
-            <FaInstagram className="text-[#8A9B8E]" />
-          </button>
-        </div>
-      </div>
-    </div>
+      </Container>
+    </section>
   );
 };
 
