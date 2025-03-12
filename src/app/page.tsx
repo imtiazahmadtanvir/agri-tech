d13156726d36e031a4e529864ccd53d70495d5d4
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-03-13 01:32:15 +0600

    feat(components): add TopInfoBar component and update HomePage content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import HomePage from "@/components/home-page/HomePage";
+import TopInfoBar from "@/components/shared/TopInfoBar";
 
 export default function Home() {
   return (
     <div className="">
+      <TopInfoBar />
       <HomePage />
     </div>
   );
