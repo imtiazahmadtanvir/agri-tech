cc3cb5c79898f433e08a9e2c0d66a3b0540bd0b0
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-03-13 02:25:33 +0600

    style: update TopInfoBar styles for improved aesthetics

--- src/components/shared/TopInfoBar.tsx
+++ src/components/shared/TopInfoBar.tsx
@@ -6,7 +6,7 @@ const TopInfoBar = () => {
   return (
     <section className="bg-[#0D401C] ">
       <Container className="flex items-center px-4 justify-between text-white py-5">
-        <div className="bg-[#235130] w-fit flex items-center gap-4 py-1 px-2 rounded-2xl">
+        <div className="bg-[#FFFFFF17] w-fit flex items-center gap-4 py-2 px-5 font-normal text-sm rounded-3xl">
           <h5>Welcome to AgriTech</h5>
           <span className="size-1.5 bg-[#F8C32C] rounded-full"></span>
           <h5>Agriculture & Organic Farms </h5>
@@ -25,7 +25,7 @@ const TopInfoBar = () => {
                 ></Image>
               </div>
 
-              <p>+1 987 654 3210</p>
+              <p className="font-normal text-sm">+1 987 654 3210</p>
             </div>
             <div className="flex items-center gap-2">
               <div className="p-2.5 bg-[#F8C32C] rounded-full w-fit">
@@ -38,7 +38,7 @@ const TopInfoBar = () => {
                 ></Image>
               </div>
 
-              <p>Donalfarms@gmail.com</p>
+              <p className="font-normal text-sm">Donalfarms@gmail.com</p>
             </div>
             <div className="flex items-center gap-2">
               <div className="px-2.5 py-2 bg-[#F8C32C] rounded-full w-fit">
@@ -51,13 +51,15 @@ const TopInfoBar = () => {
                 ></Image>
               </div>
 
-              <p>Prinsengracht 250, Amsterdam Netherlands</p>
+              <p className="font-normal text-sm">
+                Prinsengracht 250, Amsterdam Netherlands
+              </p>
             </div>
           </div>
           {/* social icon */}
-          <div className="bg-[#235130] px-6 py-3 w-fit flex items-center rounded-3xl gap-4">
+          <div className="bg-[#FFFFFF17] py-2 px-5  w-fit flex items-center rounded-3xl gap-4">
             <button>
-              <FaFacebook className="text-[#8A9B8E]" />
+              <FaFacebook className="text- text-[#8A9B8E]" />
             </button>
             <button>
               <FaTwitter className="text-[#8A9B8E]" />
