bf06521ff31c2c8b371e8cedf57e27efdd9f6de4
tarek <sitarek77@gmail.com>
2025-03-13 23:40:22 +0600

    Contact Page add and modifyedn

--- src/components/shared/contact-section/ContactUs.tsx
+++ src/components/shared/contact-section/ContactUs.tsx
@@ -1,9 +1,23 @@
-const ContactUs = () => {
-    return (
-        <section>
-            
-        </section>
-    );
+import React from "react";
+import {
+  FaMapMarkerAlt,
+  FaEnvelope,
+  FaPhoneAlt,
+  FaClock,
+} from "react-icons/fa";
+import ContactInfo from "./ContactInfo";
+import ContactForm from "./ContactForm";
+
+const ContactUs: React.FC = () => {
+  return (
+    <div className="gap-16 grid grid-cols-1 md:grid-cols-2 mx-auto my-20 w-full max-w-4xl min-h-screen">
+      {/* Left Section - Image & Address */}
+      <ContactInfo />
+
+      {/* Right Section - Contact Form */}
+      <ContactForm />
+    </div>
+  );
 };
 
 export default ContactUs;
