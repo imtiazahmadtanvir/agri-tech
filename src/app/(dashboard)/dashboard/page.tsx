6644d26778e0267ee91c4ae742bca279e767fea5
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-03-22 12:54:10 +0600

    refactor: remove Weather component and WebSocket functionality from dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,8 @@
-import Weather from "@/components/weather-alart/Weather";
-
 export default async function DashboardPage() {
   return (
     <div>
       <h2>Dashboard Content</h2>
       <p>Welcome to your dashboard!</p>
-      <Weather></Weather>
     </div>
   );
 }
