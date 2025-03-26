d8bab569c3ec1c1ef7da881a39a663600c771a98
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-03-26 16:44:41 +0600

    feat(market): implement Marketplace context and layout structure

---
+++ src/context/MarketplaceContext.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { createContext, ReactNode, useContext, useState } from "react";
+interface MarketplaceContextType {
+  minPrice: number;
+  maxPrice: number;
+  selectedCategories: string[];
+  activeSection: string;
+  setMinPrice: (value: number) => void;
+  setMaxPrice: (value: number) => void;
+  setSelectedCategories: (categories: string[]) => void;
+  setActiveSection: (section: string) => void;
+}
+const MarketplaceContext = createContext<MarketplaceContextType | undefined>(
+  undefined
+);
+export default function MarketplaceProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [minPrice, setMinPrice] = useState<number>(0);
+  const [maxPrice, setMaxPrice] = useState<number>(1000);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [activeSection, setActiveSection] = useState<string>("products");
+
+  return (
+    <MarketplaceContext.Provider
+      value={{
+        minPrice,
+        maxPrice,
+        selectedCategories,
+        activeSection,
+        setMaxPrice,
+        setMinPrice,
+        setActiveSection,
+        setSelectedCategories,
+      }}
+    >
+      {children}
+    </MarketplaceContext.Provider>
+  );
+}
+export function useMarketPlace() {
+  const context = useContext(MarketplaceContext);
+  if (!context) {
+    throw new Error("useMarketplace must be used within a MarketplaceProvider");
+  }
+  return context;
+}
