8fd58d265774662508a30481d8c4605fc2621060
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-03-26 11:41:46 +0600

    feat(market): refactor SupplyMarket to Marketplace and enhance filter options with categories and price range inputs

--- src/app/market/buyer-seller/page.tsx
+++ src/app/market/buyer-seller/page.tsx
@@ -2,16 +2,68 @@ import ContainerSmall from "@/components/shared/max-w-container/ContainerSmall";
 import React from "react";
 import { IoSearch } from "react-icons/io5";
 
-export default function SupplyMarket() {
+export default function Marketplace() {
+  const categories = [
+    "Crops",
+    "Fruits",
+    "Vegetables",
+    "Livestock",
+    "Dairy Products",
+    "Poultry",
+    "Fish & Aquaculture",
+    "Organic Produce",
+    "Herbs & Medicinal Plants",
+    "Other",
+  ];
   return (
-    <ContainerSmall className="flex">
-      <aside>
+    <ContainerSmall className="flex gap-6 py-6">
+      {/* sidebar */}
+      <aside className="w-1/4">
+        <h3 className="text-lg font-semibold mb-4">Filters</h3>
+        {/* price range */}
         <div>
-          <h3>Filter by price</h3>
-          <input max={1000} min={0} type="range" name="" id="" />
+          <h4>Price Range</h4>
+          <div className="flex items-center gap-2 mb-2">
+            <input
+              placeholder="Min"
+              className="w-1/2 border p-2"
+              type="number"
+              name=""
+              id=""
+            />
+            <input
+              placeholder="Max"
+              className="w-1/2 border p-2"
+              type="number"
+              name=""
+              id=""
+            />
+          </div>
+          <input
+            max={1000}
+            min={0}
+            type="range"
+            className="w-full accent-blue-500"
+            name=""
+            id=""
+          />
+          <p></p>
         </div>
+        {/* categories */}
+        <div className="mb-6">
+          <h4>Categories</h4>
+          {categories.map((cat) => (
+            <label key={cat} className="flex items-center mb-2">
+              <input type="checkbox" name="" id="" className="mr-2" />
+              <span>{cat}</span>
+            </label>
+          ))}
+        </div>
+        {/* clear Filters */}
+        <button>Clear All Filter</button>
       </aside>
-      <div className="w-full">
+      {/* main content */}
+      <div className="w-3/4">
         <section className="flex justify-between items-center">
           <div>
             <select className="border px-2.5 py-3" name="sort" id="">
