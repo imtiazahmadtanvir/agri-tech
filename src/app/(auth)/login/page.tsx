ca0fdcfa89c0942a024ac2fb667c7b2d90a40377
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-04-08 07:54:41 +0600

    fix(LoginPage): replace loading message with LoadingSpinner component

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,16 @@
 import Login from "@/components/auth/Login";
+import LoadingSpinner from "@/components/spinner/LoadingSpinner";
 import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div>
+          <LoadingSpinner />
+        </div>
+      }
+    >
       <Login />
     </Suspense>
   );
