4005f5a150c7076b911947ef477aa7d5fc6a3ad8
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-04-17 18:51:55 +0600

    feat(Marketplace): implement marketplace data fetching and display with improved table layout

--- src/app/(dashboard)/adminDashboard/marketplace/page.tsx
+++ src/app/(dashboard)/adminDashboard/marketplace/page.tsx
@@ -1,3 +1,85 @@
-export default function Marketplace() {
-  return <div>Marketplace</div>;
+import axios from "axios";
+import { FileEdit } from "lucide-react";
+import { headers } from "next/headers";
+import Image from "next/image";
+interface Item {
+  _id: string;
+  productName: string;
+  category: string;
+  price: number;
+  location: string;
+  phoneNumber: string;
+  listed: string;
+  verifyStatus: boolean;
+  userName: string;
+  photos: string[];
+}
+export default async function Marketplace() {
+  let items: Item[] = [];
+  try {
+    const res = await axios.get(
+      `${process.env.NEXTAUTH_URL}/api/adminDashboard/marketplace`,
+      {
+        headers: Object.fromEntries(await headers()),
+      }
+    );
+    items = res.data.data;
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(items);
+  return (
+    <div>
+      <table className="min-w-full divide-y divide-gray-200 text-sm text-left ">
+        <thead className="bg-green-100">
+          <tr>
+            <th className="px-4 py-2">Image</th>
+            <th className="px-4 py-2">Product</th>
+            <th className="px-4 py-2">Category</th>
+            <th className="px-4 py-2">Price</th>
+            <th className="px-4 py-2">User</th>
+            <th className="px-4 py-2">Status</th>
+            <th className="px-4 py-2">Actions</th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-100">
+          {items.map((item) => (
+            <tr key={item._id}>
+              <td>
+                <Image
+                  src={item.photos[0] || ""}
+                  alt={item.productName}
+                  height={60}
+                  width={60}
+                />
+              </td>
+              <td className="px-4 py-2">{item.productName}</td>
+              <td className="px-4 py-2">{item.category}</td>
+              <td className="px-4 py-2">{item.price} ৳</td>
+              <td className="px-4 py-2">{item.userName}</td>
+              <td className="px-4 py-2">
+                <span
+                  className={`inline-block px-2 py-1 rounded text-xs ${
+                    item.verifyStatus
+                      ? "bg-green-200 text-green-800"
+                      : "bg-red-200 text-red-800"
+                  }`}
+                >
+                  {item.verifyStatus ? "Verified" : "Unverified"}
+                </span>
+              </td>
+              <td className="px-4 py-2">
+                <button className=" text-white px-3 py-1 rounded mr-2">
+                  <FileEdit className="text-green-400" />
+                </button>
+                <button className="bg-red-500 text-white px-3 py-1 rounded">
+                  View
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- src/app/api/adminDashboard/marketplace/route.ts
+++ src/app/api/adminDashboard/marketplace/route.ts
@@ -1,8 +1,9 @@
 import { authOptions } from "@/lib/auth"
+import dbConnect, { collectionNameObj } from "@/lib/dbConnect"
 import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server"
 
-export const GET = async (res: Request) => {
+export const GET = async () => {
     try {
         const session = await getServerSession(authOptions)
         if (session?.user.role !== "admin") {
@@ -10,9 +11,22 @@ export const GET = async (res: Request) => {
                 message: 'unauthorize access'
             }, { status: 401 })
         }
-        console.log(session?.user.role);
-        return NextResponse.json({ success: true, message: "welcome" }, { status: 200 })
 
+        const listingsCollection = await dbConnect(collectionNameObj.listingsCollection)
+        const result = await listingsCollection.find({ verifyStatus: false }, {
+            projection: {
+                productName: 1,
+                category: 1,
+                price: 1,
+                location: 1,
+                listed: 1,
+                verifyStatus: 1,
+                phoneNumber: 1,
+                userName: 1,
+                photos: 1
+            }
+        }).toArray()
+        return NextResponse.json({ success: true, message: "fetch successful", data: result }, { status: 200 })
     } catch (error) {
         console.log("error in dashboard get marketplace route", error);
         return NextResponse.json({
