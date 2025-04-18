dcef854def95609308ce09e146f392f3d3e7cd30
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-04-18 10:03:40 +0600

    fix(route): ensure session validation and error logging in GET handler

--- src/app/api/adminDashboard/marketplace/[id]/route.ts
+++ src/app/api/adminDashboard/marketplace/[id]/route.ts
@@ -1,15 +1,24 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export const GET = async (
     req: Request,
     { params }: { params: Promise<{ id: string }> }
 ) => {
-    const resolvedParams = await params;
-    const { id } = resolvedParams
-    console.log(id);
     try {
+        const resolvedParams = await params;
+        const { id } = resolvedParams
+        console.log(id);
+        const session = await getServerSession(authOptions)
+        if (session?.user.role !== "admin") {
+            return NextResponse.json({
+                message: 'unauthorize access'
+            }, { status: 401 })
+        }
         return NextResponse.json({ message: "Check console" });
     } catch (error) {
+        console.error('error from product details', error)
         return NextResponse.json({ message: "Something went wrong", }, { status: 500 });
     }
 };
