b6bdf641952c58bd41a84a08ec4c9d98984ac77c
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-04-18 09:55:49 +0600

    fix(route): update parameter type to Promise<{ id: string }> for async handling

--- src/app/api/adminDashboard/marketplace/[id]/route.ts
+++ src/app/api/adminDashboard/marketplace/[id]/route.ts
@@ -2,13 +2,12 @@ import { NextResponse } from "next/server";
 
 export const GET = async (
     req: Request,
-    { params }: { params: { id: string } }
+    { params }: { params: Promise<{ id: string }> }
 ) => {
     const resolvedParams = await params;
     const { id } = resolvedParams
     console.log(id);
     try {
-
         return NextResponse.json({ message: "Check console" });
     } catch (error) {
         return NextResponse.json({ message: "Something went wrong", }, { status: 500 });
