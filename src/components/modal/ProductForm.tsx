385f44fec4f3254a538c7fbaa6aec71e5147faf8
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-04-22 21:44:51 +0600

    feat(ProductForm): add modal functionality and improve layout for better user experience

--- src/components/modal/ProductForm.tsx
+++ src/components/modal/ProductForm.tsx
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
+
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
+
 export default function ProductForm({ isOpen, onClose }: ModalProps) {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
-  return <div className="fixed inset-0 bg-black/20 z-50">hi</div>;
+
+  return (
+    <div
+      className="fixed inset-0 flex items-center justify-center bg-black/20 z-50"
+      onClick={onClose}
+    >
+      <div
+        className="bg-white rounded-lg shadow-lg w-[90%] max-w-md relative px-4 py-6"
+        onClick={(e) => e.stopPropagation()}
+      >
+        f
+      </div>
+    </div>
+  );
 }
