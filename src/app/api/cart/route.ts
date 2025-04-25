080754fa9f3c092357d27bc54de79c3fc25e16b1
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-04-25 13:19:02 +0600

    feat (api) added get api for cart

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,10 +1,68 @@
-import { NextRequest } from "next/server";
+import { authOptions } from "@/lib/auth";
+import dbConnect, { collectionNameObj } from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+interface CartItem {
+    productId: string;
+    quantity: number;
+}
 
 export const POST = async (req: NextRequest) => {
     try {
         const { productId, quantity } = await req.json()
-        console.log(productId);
-    } catch (error) {
+        const session = await getServerSession(authOptions)
+        console.log(session);
+        if (!session?.user?.email) {
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        }
+        const userEmail = session?.user.email
+        const cartsCollection = await dbConnect(collectionNameObj.cartsCollection)
+        const existingCart = await cartsCollection.findOne({ userEmail });
+        if (existingCart) {
+            const existingItem = existingCart.items.find((item: CartItem) => item.productId === productId)
+            if (existingItem) {
+                await cartsCollection.updateOne({ userEmail, "items.productId": productId }, {
+                    $inc: { "items.$.quantity": quantity }
+                });
+            } else {
+                const updatedItems = [
+                    ...existingCart.items,
+                    { productId, quantity }
+                ];
+                await cartsCollection.updateOne({ userEmail }, {
+                    $set: { items: updatedItems }
 
+                })
+            }
+        } else {
+            await cartsCollection.insertOne({
+                userEmail,
+                items: [{ productId, quantity }]
+            })
+        }
+
+
+        return NextResponse.json({ success: true }, { status: 200 });
+    } catch (error) {
+        console.error("Cart POST error:", error);
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
+export const GET = async () => {
+    try {
+        const session = await getServerSession(authOptions)
+        console.log(session);
+        if (!session?.user?.email) {
+            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+        }
+        const userEmail = session?.user.email
+        const cartsCollection = await dbConnect(collectionNameObj.cartsCollection)
+        const cart = await cartsCollection.findOne({ userEmail });
+        return NextResponse.json({ cart: cart?.items || [] })
+
+    } catch (error) {
+        console.error("GET /cart error:", error);
+        return NextResponse.json({ message: "Internal server error" }, { status: 500 });
     }
 }
