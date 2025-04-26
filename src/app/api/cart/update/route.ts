66aec53b48bf95af219c04e94b420b35ef7c1991
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-04-26 23:43:47 +0600

    feat:Added api for updata product quantity

--- src/app/api/cart/update/route.ts
+++ src/app/api/cart/update/route.ts
@@ -1,6 +1,24 @@
-import { NextRequest } from "next/server";
+import { authOptions } from "@/lib/auth";
+import dbConnect, { collectionNameObj } from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export const PATCH = async (req: NextRequest) => {
-    const { quantity } = await req.json()
+    const { quantity, productId } = await req.json()
     console.log(quantity);
+    const session = await getServerSession(authOptions)
+    if (!session?.user.email || !productId || typeof quantity !== 'number') {
+        return NextResponse.json({ message: "Invalid request" }, { status: 400 })
+    }
+    const cart = await dbConnect(collectionNameObj.cartsCollection)
+    try {
+        await cart.updateOne({
+            userEmail: session.user.email, "items.productId": productId
+        }, { $set: { "items.quantity": quantity } })
+        return NextResponse.json({ message: "Quantity updated successfully" }, { status: 200 })
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ message: "Internal server error" }, { status: 500 })
+
+    }
 }
