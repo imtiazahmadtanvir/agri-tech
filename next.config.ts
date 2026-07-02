c52ab40c55ef5b15ce37a196efe2e164931a7706
imtiazahmadtanvir <imtiaztanveer07@gmail.com>
2026-07-02 15:52:57 +0600

    feat: allow image loading from Unsplash in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,10 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "openweathermap.org",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
